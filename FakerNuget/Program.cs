dadb1f829b99752291c6ec6d39ce29092657e34f
Määttä Eetu <s2001019@edu.bc.fi>
2022-02-15 13:39:15 +0200

    Initial Commit

---
+++ FakerNuget/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Faker;
+
+namespace FakerNuget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GeneratePerson();
+            GeneratePerson();
+            GeneratePerson();
+
+            Console.WriteLine("Press any key to close this window...");
+            Console.ReadLine();
+        }
+
+        static void GeneratePerson()
+        {
+            var firstName = Faker.Name.First();
+            var lastName = Faker.Name.Last();
+            var address = Faker.Address.StreetAddress();
+            var city = Faker.Address.City();
+            var number = Faker.RandomNumber.Next(100);
+            var dob = Faker.Identification.DateOfBirth();
+
+            var ssn = Faker.Identification.SocialSecurityNumber();
+            var mbi = Faker.Identification.MedicareBeneficiaryIdentifier();
+            var usPassport = Faker.Identification.UsPassportNumber();
+
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+            Console.WriteLine(address);
+            Console.WriteLine(city);
+            Console.WriteLine(number);
+            Console.WriteLine(dob);
+            Console.WriteLine(ssn);
+            Console.WriteLine(mbi);
+            Console.WriteLine(usPassport);
+            Console.WriteLine("");
+        }
+    }
+}
